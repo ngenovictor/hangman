9f67fb349f36ede7a22f52e940500715956e472b
Victor Ngeno <ngenovictor321@gmail.com>
2017-08-03 15:09:38 +0300

    write test for instance of game method

---
+++ src/main/java/Game.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kingkong on 8/3/17.
+ */
+public class Game {
+    String computeChoices(){
+        return "";
+    }
+}
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,7 +1,15 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by kingkong on 8/3/17.
  */
 public class HangmanTest {
+    @Test
+    public void getSomething_returnsSomething_(){
+//        check if class Game exists
+        Game testGame = new Game();
+        String computerChoice = testGame.computeChoices();
+        assertEquals(true, computerChoice instanceof String);
+    }
 }
